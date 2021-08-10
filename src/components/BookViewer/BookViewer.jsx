1214ecfae026b7cdda39a1c1e5403187feca5fac
Ryan <ryan@ryannd.com>
2021-08-09 21:05:04 -0500

    Separated book view display into separate 'Book' component

--- src/components/BookViewer/BookViewer.jsx
+++ src/components/BookViewer/BookViewer.jsx
@@ -1,4 +1,6 @@
+import { tsPropertySignature } from '@babel/types';
 import React from 'react';
+import Book from '../Book/Book'
 
 const BookViewer = (props) => {
     return (
@@ -9,8 +11,7 @@ const BookViewer = (props) => {
             </div>
             <div className="col-md-4">
                 {/* Display book with cover */}
-                <h1>{props.book.title}</h1>
-                <h4>{props.book.author}</h4>
+                <Book book={props.book} />
             </div>
             <div className="col-md-4">
                 {/* Button here to move to the previous book viewed */}
